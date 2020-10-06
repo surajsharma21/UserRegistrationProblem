508f1d84b0738b7107de300e50256de79871f992
Suraj <surajshrm000@gmail.com>
2020-10-06 22:37:18 +0530

    [Suraj] Add: Validating password having atleast one uppercase

--- UserRegistrationProblem/RegexValidation.cs
+++ UserRegistrationProblem/RegexValidation.cs
@@ -11,8 +11,7 @@ namespace UserRegistrationProblem
         public const string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public const string Regex_MobileNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
-        //  \S matches any non-whitespace character
-        public const string Regex_Password = "[\\S]{8,}";
+        public const string Regex_Password = "[\\S]{8}";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, Regex_FirstName);
