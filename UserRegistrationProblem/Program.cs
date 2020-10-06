ac6efd7934b0bbc165b84e6c9ae4e09461e19311
Suraj <surajshrm000@gmail.com>
2020-10-06 22:07:47 +0530

    [Suraj] Add: Welcome Message

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem!");
+        }
+    }
+}
